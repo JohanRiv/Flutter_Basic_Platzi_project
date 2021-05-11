eb437a135d00a88196a696eeecf29dcb74b64f47
JohanRiv <johancamiloandres@hotmail.com>
2021-05-11 11:11:30 -0500

    Ajustes rows stars

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
+  double starsCount;
+  String titleText;
+  String descriptionText;
+
+  DescriptionPlace(this.titleText, this.descriptionText, this.starsCount);
+
   @override
   Widget build(BuildContext context) {
-    var stars = Container(
+    var listStars = <Widget>[];
+
+    Container starsWidgetFull = Container(
       margin: EdgeInsets.only(top: 322, right: 2),
       child: Icon(
         Icons.star,
@@ -11,25 +19,66 @@ class DescriptionPlace extends StatelessWidget {
       ),
     );
 
-    return Row(
+    Container starsWidgetHalf = Container(
+      margin: EdgeInsets.only(top: 322, right: 2),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFFDD835),
+      ),
+    );
+
+    Container starsWidgetNone = Container(
+      margin: EdgeInsets.only(top: 322, right: 2),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFe0e0e0),
+      ),
+    );
+
+    Expanded titleWidget = Expanded(
+        child: Container(
+      margin: EdgeInsets.only(top: 320, left: 20, right: 15),
+      child: Text(
+        titleText,
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+        textAlign: TextAlign.left,
+      ),
+    ));
+
+    Expanded descriptionWidget = Expanded(
+        child: Container(
+      margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: Text(
+        descriptionText,
+        style: TextStyle(fontSize: 20),
+        textAlign: TextAlign.left,
+      ),
+    ));
+
+    for (var i = 0; i < 5; i++) {
+      if (i < starsCount.toInt()) {
+        listStars.add(starsWidgetFull);
+      } else {
+        if (starsCount <= i && (starsCount - starsCount) == 0) {
+          listStars.add(starsWidgetNone);
+        } else {
+          listStars.add(starsWidgetHalf);
+        }
+      }
+    }
+
+    return Column(
       children: [
-        Container(
-          margin: EdgeInsets.only(top: 320, left: 20, right: 15),
-          child: Text(
-            "Caño Cristales",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
-            textAlign: TextAlign.left,
-          ),
-        ),
         Row(
           children: [
-            stars,
-            stars,
-            stars,
-            stars,
-            stars,
+            titleWidget,
+            Expanded(
+                child: Row(
+              children: listStars,
+            ))
           ],
-        )
+        ),
+        descriptionWidget,
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,10 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('Trip App'),
           ),
-          body: new DescriptionPlace(),
+          body: new DescriptionPlace(
+              "Caño Cristales",
+              "Caño Cristales (La Macarena, Meta, Colombia) es considerado por muchos como el “Río más Hermoso del Mundo”. Pareciera exagerado el cumplido, pero solamente visitándolo es posible dimensionar la belleza de este paraíso colombiano. Durante muchos años ha estado totalmente cerrado al turismo. Pero hoy es posible visitarlo con total tranquilidad y con vuelos directos desde Bogotá y Villavicencio.",
+              4.5),
         ));
   }
 }
